d45279389d6b3c119c6e4f49979ef07766cc7819
Harman Singh <harmansingh@me.com>
2016-08-22 17:58:05 -0700

    Created database and added random values.
    * defined database and table
    
    * Added random values to database

---
+++ schema.sql
@@ -0,0 +1,66 @@
+DROP DATABASE IF EXISTS bookstore;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  published DATE NOT NULL,
+  fiction BOOLEAN NOT NULL,
+  image_link VARCHAR(255),
+  retail_price VARCHAR(255),
+  buy_link VARCHAR(255),
+  description VARCHAR(500)
+);
+
+DROP TABLE IF EXISTS genres;
+
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS book_genres;
+
+CREATE TABLE book_genres (
+  book_id INTEGER PRIMARY KEY,
+  genre_id INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS book_authors;
+
+CREATE TABLE book_authors (
+  book_id INTEGER PRIMARY KEY,
+  author_id INTEGER NOT NULL
+);
+
+--Fixture Data
+
+INSERT INTO
+  genres (name)
+VALUES
+  ('Economics'),
+  ('Fantasy'),
+  ('Romance'),
+  ('Horror'),
+  ('Sci-Fi'),
+  ('Historical Drama');
+
+INSERT INTO
+  books (title, published, fiction)
+VALUES
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false),
+  ('blasd kdjsfkjsdf jsdkfksdf', now(), false);
